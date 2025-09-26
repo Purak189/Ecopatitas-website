5c0ced1e2d834c4c66911c29772da09f8dafcadc
Jair Velasquez <jairvelazpizar@gmail.com>
2025-09-26 18:15:30 -0500

    J J J

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,7 +6,6 @@ import { DvFooterComponent } from './dv-footer/dv-footer.component';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { DvAboutUsComponent } from './dv-about-us/dv-about-us.component';
 import { DvModalWspComponent } from './dv-modal-wsp/dv-modal-wsp.component';
-import { DvBannerCardsComponent } from './dv-banner-cards/dv-banner-cards.component';
 import { DvOurSkillsComponent } from './dv-our-skills/dv-our-skills.component';
 import { DvServicesListComponent } from './dv-services-list/dv-services-list.component';
 import { DvBestProductsComponent } from './dv-best-products/dv-best-products.component';
@@ -14,11 +13,9 @@ import { DvClassicProductComponent } from './dv-classic-product/dv-classic-produ
 import { DvFusionProductComponent } from './dv-fusion-product/dv-fusion-product.component';
 import { DvImageComponent } from './dv-image/dv-image.component';
 import { DvCtaComponent } from './dv-cta/dv-cta.component';
-import { DvBannerPagesComponent } from './dv-banner-pages/dv-banner-pages.component';
 import { DvOurHistoryComponent } from './dv-our-history/dv-our-history.component';
 import { DvMissionVisionComponent } from './dv-mission-vision/dv-mission-vision.component';
 import { DvOurValuesComponent } from './dv-our-values/dv-our-values.component';
-import { DvProductsListSectionComponent } from './dv-products-list-section/dv-products-list-section.component';
 import { DvTypeSelectionSectionComponent } from './dv-type-selection-section/dv-type-selection-section.component';
 
 const components = [
@@ -28,7 +25,6 @@ const components = [
 	DvAboutUsComponent,
 	DvModalWspComponent,
 	DvOurSkillsComponent,
-  DvBannerCardsComponent,
   DvServicesListComponent,
   DvBestProductsComponent,
   DvClassicProductComponent,
@@ -40,11 +36,9 @@ const components = [
   DvFusionProductComponent,
   DvImageComponent,
   DvCtaComponent,
-  DvBannerPagesComponent,
   DvOurHistoryComponent,
   DvMissionVisionComponent,
   DvOurValuesComponent,
-  DvProductsListSectionComponent,
   DvTypeSelectionSectionComponent,
 	],
   exports: [
@@ -52,11 +46,9 @@ const components = [
     DvFusionProductComponent,
     DvImageComponent,
     DvCtaComponent,
-    DvBannerPagesComponent,
     DvOurHistoryComponent,
     DvMissionVisionComponent,
     DvOurValuesComponent,
-    DvProductsListSectionComponent,
     DvTypeSelectionSectionComponent,
   ],
 	imports: [
